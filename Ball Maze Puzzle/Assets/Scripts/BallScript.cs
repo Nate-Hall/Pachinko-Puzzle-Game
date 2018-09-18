5f5808db57eaad4c6a180e6e4514d6d55a8ea56e
Unknown <nsehall.coding@gmail.com>
2018-09-17 23:32:38 -0700

    Initial Working Prototype

---
+++ Ball Maze Puzzle/Assets/Scripts/BallScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+	private bool active = false;
+	private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+
+
+
+	private void Update() {
+		if(!active) {
+			GetComponent<Rigidbody>().useGravity = false;
+			GetComponent<Rigidbody>().velocity = Vector3.zero;
+		} else {
+			GetComponent<Rigidbody>().useGravity = true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			active = !active;
+			if(!active) {
+				ResetBall();
+				Debug.Log("ResetBall");
+			}
+		}
+
+		if(transform.position.y < -10) {
+			ResetBall();
+		}
+	}
+
+
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.tag == "Goal") {
+			ResetBall();
+		}
+	}
+
+
+
+	public void ResetBall() {
+		active = false;
+		GetComponent<Rigidbody>().velocity = Vector3.zero;
+		transform.position = startPos;
+	}
+}
---
+++ Ball Maze Puzzle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private GameObject selectedTile;
+	private Vector3 selectedTilePos;
+	public GUIStyle style;
+
+	public Transform selectParticle;
+
+	public Transform frontPanel;
+	private Animator frontPanelAnimator;
+
+
+
+	private void Start() {
+		style.fontSize = 30;
+		style.normal.textColor = Color.white;
+
+		frontPanelAnimator = frontPanel.GetComponent<Animator>();
+		frontPanelAnimator.SetTrigger("OpenPanel");
+	}
+
+
+
+	private void OnGUI() {
+		GUI.Label(new Rect(Screen.width/2 - 200, Screen.height - 90, 200, 200), "Press \"Spacebar\" to start", style);
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0)) {
+			GameObject newTile = SelectTile();
+			if(newTile == null || newTile == selectedTile) {
+				selectedTile = null;
+				selectParticle.GetComponent<ParticleSystem>().Stop();
+				selectParticle.position = new Vector3(0, -100, 0);
+			} else if(selectedTile == null) {
+				selectedTile = newTile;
+				selectedTilePos = selectedTile.transform.position;
+				selectParticle.position = new Vector3(selectedTilePos.x, selectedTilePos.y, 0);
+				selectParticle.GetComponent<ParticleSystem>().Play();
+			} else {
+				selectedTile.transform.position = newTile.transform.position;
+				newTile.transform.position = selectedTilePos;
+				selectedTile = null;
+				selectParticle.GetComponent<ParticleSystem>().Stop();
+				selectParticle.position = new Vector3(0, -100, 0);
+			}
+
+		}
+	}
+
+
+
+	private GameObject SelectTile() {
+		RaycastHit hit = new RaycastHit();
+		if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, float.MaxValue, 1 << 8)) {
+			return hit.transform.gameObject;
+		}
+		Debug.Log("Pressed");
+		return null;
+	}
+}
